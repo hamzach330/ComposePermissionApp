bec78a88afa7cc91032f75a7fa3388b7a7e11943
Hamza Masood <hamzach330@gmail.com>
2023-02-13 02:18:27 +0100

    Added View and ViewModel

--- app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.permissionapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.permissionapp.ui.theme.PermissionAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +18,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PermissionAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val viewModel = viewModel<MainViewModel>()
+                val dialogQueue = viewModel.visiblePermissionDialogQueue
+
+                //// View ////
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(
+                            text = "Request For Single Permission", 
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    
+                    Spacer(modifier = Modifier
+                        .height(10.dp)
+                    )
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(
+                            text = "Request For Multi Permission",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+
+
                 }
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PermissionAppTheme {
-        Greeting("Android")
     }
 }
